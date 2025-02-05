f6ece397babed8444e0d85b6e3caf8999eb8df5c
24363348 <yapri177@gmail.com>
2025-02-05 14:14:38 +0700

    remove css not use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 "use client";
-import "jsvectormap/dist/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
 import "@/assets/css/style.css";
 import React, { useEffect, useState } from "react";
 import Loader from "@/components/common/Loader";
